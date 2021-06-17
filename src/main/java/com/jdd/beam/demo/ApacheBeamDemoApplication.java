3314874e2184e1c3ae6755247bc4df9b0da21035
jondoetsch27 <jonathon.doetsch@gmail.com>
2021-06-17 10:08:32 -0700

    06/17 - added ApacheBeamDemoApplication.java @jondoetsch27

---
+++ src/main/java/com/jdd/beam/demo/ApacheBeamDemoApplication.java
@@ -0,0 +1,12 @@
+package com.jdd.beam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheBeamDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApacheBeamDemoApplication.class, args);
+  }
+}
