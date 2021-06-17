d82d04968e1bce0992cc810294c9e13931304294
jondoetsch27 <jonathon.doetsch@gmail.com>
2021-06-17 12:01:54 -0700

    06/17 - transform layer @jondoetsch27

---
+++ src/main/java/com/jdd/beam/demo/transform/CountWords.java
@@ -0,0 +1,18 @@
+package com.jdd.beam.demo.transform;
+
+import com.jdd.beam.demo.process.ExtractWordsFn;
+import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+
+public class CountWords extends PTransform<PCollection<String>, PCollection<KV<String, Long>>> {
+
+  @Override
+  public PCollection<KV<String, Long>> expand(PCollection<String> lines) {
+    PCollection<String> words = lines.apply(ParDo.of(new ExtractWordsFn()));
+    PCollection<KV<String, Long>> wordCounts = words.apply(Count.perElement());
+    return wordCounts;
+  }
+}
